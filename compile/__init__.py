5930b434be206b8813961fd04f30428452b7c3af
pyos <pyos100500@gmail.com>
2013-04-06 19:50:06 +0400

    Add support for compiler extensions and the first one of them.

--- compile/__init__.py
+++ compile/__init__.py
@@ -1,3 +1,7 @@
+import os
+import imp
+import marshal
+
 from .  import core
 from .. import parse
 
@@ -31,3 +35,31 @@ def fd(fd, name='<stream>'):
 def it(text, name='<string>'):
 
     return ast(parse.it(text, name))
+
+
+for f in [
+       'shortcuts.dg'
+#  , 'conditionals.dg',      'unary.dg'
+#  ,       'binary.dg', 'comparison.dg'
+#  ,      'inherit.dg',     'switch.dg', 'where.dg'
+#  ,        'loops.dg',     'unsafe.dg',  'with.dg', 'yield.dg'
+#  ,      'imphook.dg', 'functional.dg'
+]:
+    f = os.path.join(__path__[0], f)
+    q = imp.cache_from_source(f)
+
+    try:
+
+        c = os.stat(q).st_mtime > os.stat(f).st_mtime and marshal.load(open(q, 'rb'))
+
+    except Exception:
+
+        c = None
+
+    if not c:
+
+        c = fd(open(f))
+        os.makedirs(os.path.dirname(q), exist_ok=True)
+        marshal.dump(c, open(q, 'wb'))
+
+    eval(c, {'__package__': __package__})
